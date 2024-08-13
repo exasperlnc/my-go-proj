98aad02884186af61dd9dcf30d43ac5a74d4cf7a
Logan Cole <emtlogancole@gmail.com>
2024-08-13 14:51:32 -0500

    refactor tests

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,11 +3,26 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("array of 5 numbers", func(t *testing.T) {
+			
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("slice of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+
+func assertCorrectMessage(t *testing.T, got, want int) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
