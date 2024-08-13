0e0575b2762132b26a7c5d97a0d2281a4bcb502f
Logan Cole <emtlogancole@gmail.com>
2024-08-13 10:41:20 -0500

    add spanish functionality to hello function

--- hello.go
+++ hello.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("Logan"))
+	fmt.Println(Hello("Logan", ""))
 }
+const exclamation = "!"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
-func Hello(name string) string {
-	englishHelloPrefix := "Hello, "
-	exclamation := "!"
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	} 
+	if language == spanish {
+		return spanishHelloPrefix + name + exclamation
+	}
 	return englishHelloPrefix + name + exclamation
 }
