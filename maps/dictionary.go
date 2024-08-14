9d887e1cfbb884dd26b0d51ef327dcb0a3b77840
Logan Cole <emtlogancole@gmail.com>
2024-08-14 16:43:46 -0500

    test Search func

---
+++ maps/dictionary.go
@@ -0,0 +1,7 @@
+package maps
+
+
+// Search takes a dictionary and a search key and returns the value associated with that key.
+func Search (dictionary map[string]string, key string) string {
+	return ""
+}
