c69f4276f401345f4a8281ace049d03161ef3e6f
Logan Cole <emtlogancole@gmail.com>
2024-08-14 16:03:48 -0500

    Wallet will not withdraw if balance is insufficient

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,14 +1,22 @@
 package pointers_errors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int 
 
 type Wallet struct {
 	balance Bitcoin
 }
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+	
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
