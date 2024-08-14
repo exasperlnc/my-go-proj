84d970a90a90f6728c4e3c6f2177d30605569de2
Logan Cole <emtlogancole@gmail.com>
2024-08-14 12:17:35 -0500

    create and test SumAll func

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("array of 5 numbers", func(t *testing.T) {
@@ -9,6 +12,17 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		want := 15
 		assertCorrectMessage(t, got, want)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("SumAll test", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 }
 
