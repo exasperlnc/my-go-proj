753d967da0a9a7cb62dc654a7368caf6a5a07f1b
Logan Cole <emtlogancole@gmail.com>
2024-08-20 13:28:09 -0500

    update dictionary with custom type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,8 @@
 package maps
 
+type Dictionary map[string]string
 
 // Search takes a dictionary and a search key and returns the value associated with that key.
-func Search (dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
