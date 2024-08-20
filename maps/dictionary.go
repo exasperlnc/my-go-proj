ecc3d01157e0afe51974cf8fca221a27cd9a5a7b
Logan Cole <emtlogancole@gmail.com>
2024-08-20 14:22:57 -0500

    add word checking functionality to dictionary.add

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,8 +4,11 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-// Search takes a dictionary and a search key and returns the value associated with that key.
+var (
+ ErrNotFound = errors.New("could not find the word you were looking for")
+ ErrWordExists = errors.New("cannot add word because it already exists")
+)
+ // Search takes a dictionary and a search key and returns the value associated with that key.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -15,6 +18,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err{
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default: 
+		return err
+	}
+
+	return nil
 }
