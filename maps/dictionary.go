81d9302a7ec0e7693983d0d5a254c37125b15c03
Logan Cole <emtlogancole@gmail.com>
2024-08-20 13:41:44 -0500

    dictionary now returns error when word not found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,8 +1,15 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
 // Search takes a dictionary and a search key and returns the value associated with that key.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
