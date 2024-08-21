236e1efb7279bf128036ae4f878cd74567fa639d
Logan Cole <emtlogancole@gmail.com>
2024-08-21 16:32:57 -0500

    refactor countdown

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -9,6 +9,10 @@ import (
 const countdownStart = 3
 const finalWord = "Go!"
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(time.Duration)
+}
 type Sleeper interface {
 	Sleep()
 }
@@ -26,15 +30,45 @@ func (d *DefaultSleeper) Sleep() {
 func (s *SpySleeper) Sleep() {
 	s.Calls ++
 }
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i >0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}	
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}	
 	Countdown(os.Stdout, sleeper)
 }
